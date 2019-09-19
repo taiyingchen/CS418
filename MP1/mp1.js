fa22ae2d92b49709dc2023b517f89d64aafa1461
Taiying Chen <taiying.tychen@gmail.com>
2019-09-18 21:22:02 -0500

    MP1: Finish my animation

--- MP1/mp1.js
+++ MP1/mp1.js
@@ -34,6 +34,11 @@ var theta = 0;
 /** @global Last time record for cumputing elapsed time */
 var lastTime = 0;
 
+/** @global If true draw logo else draw my animation */
+var logo = true;
+
+/** @global If animation changes we need to reset the buffer */
+var change = false;
 
 /**
  * Creates a context for WebGL
@@ -135,7 +140,7 @@ function lerp(start, end, time) {
 /**
  * Load vertices to buffer
  */
-function loadVertices() {
+function loadVerticesLogo() {
   // Vertices to compose the U of I logo,
   // draw a large blue I, then draw a small orange I,
   // covering the blue I with the small orange I to get the blue border.
@@ -331,14 +336,14 @@ function loadVertices() {
 /**
  * Load colors to buffer
  */
-function loadColors() {
+function loadColorsLogo() {
   vertexColorBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
   var blue = [
-    19 / 256, 41 / 256, 74 / 256, 1.0
+    19/256, 41/256, 74/256, 1.0
   ];
   var orange = [
-    234 / 256, 76 / 256, 39 / 256, 1.0
+    234/256, 76/256, 39/256, 1.0
   ];
   var colors = [];
 
@@ -356,12 +361,236 @@ function loadColors() {
 /**
  * Populate buffers with data
  */
-function setupBuffers() {
+function setupBuffersLogo() {
   //Generate the vertex positions    
-  loadVertices();
+  loadVerticesLogo();
 
   //Generate the vertex colors
-  loadColors();
+  loadColorsLogo();
+}
+
+/**
+ * Load vertices to buffer
+ */
+function loadVerticesMy() {
+  var triangleVertices = [
+    // Top stand
+    -1.0, 0.7, 0.0,
+    -1.0, 1.0, 0.0,
+    0.0, 0.7, 0.0,
+    -1.0, 1.0, 0.0,
+    0.0, 0.7, 0.0,
+    1.0, 1.0, 0.0,
+    0.0, 0.7, 0.0,
+    1.0, 1.0, 0.0,
+    1.0, 0.7, 0.0,
+    // Top rack
+    -1.0, 0.6, 0.0,
+    -1.0, 0.7, 0.0,
+    -0.9, 0.7, 0.0,
+    -0.9, 0.7, 0.0,
+    -0.8, 0.6, 0.0,
+    -0.7, 0.7, 0.0,
+    -0.7, 0.7, 0.0,
+    -0.6, 0.6, 0.0,
+    -0.5, 0.7, 0.0,
+    -0.5, 0.7, 0.0,
+    -0.4, 0.6, 0.0,
+    -0.3, 0.7, 0.0,
+    -0.3, 0.7, 0.0,
+    -0.2, 0.6, 0.0,
+    -0.1, 0.7, 0.0,
+    -0.1, 0.7, 0.0,
+    0.0, 0.6, 0.0,
+    0.1, 0.7, 0.0,
+    0.1, 0.7, 0.0,
+    0.2, 0.6, 0.0,
+    0.3, 0.7, 0.0,
+    0.3, 0.7, 0.0,
+    0.4, 0.6, 0.0,
+    0.5, 0.7, 0.0,
+    0.5, 0.7, 0.0,
+    0.6, 0.6, 0.0,
+    0.7, 0.7, 0.0,
+    0.7, 0.7, 0.0,
+    0.8, 0.6, 0.0,
+    0.9, 0.7, 0.0,
+    0.9, 0.7, 0.0,
+    1.0, 0.7, 0.0,
+    1.0, 0.6, 0.0,
+    // Bottom stand
+    -1.0, -0.7, 0.0,
+    -1.0, -1.0, 0.0,
+    0.0, -0.7, 0.0,
+    -1.0, -1.0, 0.0,
+    0.0, -0.7, 0.0,
+    1.0, -1.0, 0.0,
+    0.0, -0.7, 0.0,
+    1.0, -1.0, 0.0,
+    1.0, -0.7, 0.0,
+    // Bottom rack
+    -1.0, -0.7, 0.0,
+    -0.9, -0.6, 0.0,
+    -0.8, -0.7, 0.0,
+    -0.8, -0.7, 0.0,
+    -0.7, -0.6, 0.0,
+    -0.6, -0.7, 0.0,
+    -0.6, -0.7, 0.0,
+    -0.5, -0.6, 0.0,
+    -0.4, -0.7, 0.0,
+    -0.4, -0.7, 0.0,
+    -0.3, -0.6, 0.0,
+    -0.2, -0.7, 0.0,
+    -0.2, -0.7, 0.0,
+    -0.1, -0.6, 0.0,
+    0.0, -0.7, 0.0,
+    0.0, -0.7, 0.0,
+    0.1, -0.6, 0.0,
+    0.2, -0.7, 0.0,
+    0.2, -0.7, 0.0,
+    0.3, -0.6, 0.0,
+    0.4, -0.7, 0.0,
+    0.4, -0.7, 0.0,
+    0.5, -0.6, 0.0,
+    0.6, -0.7, 0.0,
+    0.6, -0.7, 0.0,
+    0.7, -0.6, 0.0,
+    0.8, -0.7, 0.0,
+    0.8, -0.7, 0.0,
+    0.9, -0.6, 0.0,
+    1.0, -0.7, 0.0,
+  ];
+
+  var triangleVerticesLong = [
+    // Top stand
+    -1.0, 0.7-0.65, 0.0,
+    -1.0, 1.0, 0.0,
+    0.0, 0.7-0.65, 0.0,
+    -1.0, 1.0, 0.0,
+    0.0, 0.7-0.65, 0.0,
+    1.0, 1.0, 0.0,
+    0.0, 0.7-0.65, 0.0,
+    1.0, 1.0, 0.0,
+    1.0, 0.7-0.65, 0.0,
+    // Top rack
+    -1.0, 0.6-0.65, 0.0,
+    -1.0, 0.7-0.65, 0.0,
+    -0.9, 0.7-0.65, 0.0,
+    -0.9, 0.7-0.65, 0.0,
+    -0.8, 0.6-0.65, 0.0,
+    -0.7, 0.7-0.65, 0.0,
+    -0.7, 0.7-0.65, 0.0,
+    -0.6, 0.6-0.65, 0.0,
+    -0.5, 0.7-0.65, 0.0,
+    -0.5, 0.7-0.65, 0.0,
+    -0.4, 0.6-0.65, 0.0,
+    -0.3, 0.7-0.65, 0.0,
+    -0.3, 0.7-0.65, 0.0,
+    -0.2, 0.6-0.65, 0.0,
+    -0.1, 0.7-0.65, 0.0,
+    -0.1, 0.7-0.65, 0.0,
+    0.0, 0.6-0.65, 0.0,
+    0.1, 0.7-0.65, 0.0,
+    0.1, 0.7-0.65, 0.0,
+    0.2, 0.6-0.65, 0.0,
+    0.3, 0.7-0.65, 0.0,
+    0.3, 0.7-0.65, 0.0,
+    0.4, 0.6-0.65, 0.0,
+    0.5, 0.7-0.65, 0.0,
+    0.5, 0.7-0.65, 0.0,
+    0.6, 0.6-0.65, 0.0,
+    0.7, 0.7-0.65, 0.0,
+    0.7, 0.7-0.65, 0.0,
+    0.8, 0.6-0.65, 0.0,
+    0.9, 0.7-0.65, 0.0,
+    0.9, 0.7-0.65, 0.0,
+    1.0, 0.7-0.65, 0.0,
+    1.0, 0.6-0.65, 0.0,
+    // Bottom stand
+    -1.0, -0.7+0.65, 0.0,
+    -1.0, -1.0, 0.0,
+    0.0, -0.7+0.65, 0.0,
+    -1.0, -1.0, 0.0,
+    0.0, -0.7+0.65, 0.0,
+    1.0, -1.0, 0.0,
+    0.0, -0.7+0.65, 0.0,
+    1.0, -1.0, 0.0,
+    1.0, -0.7+0.65, 0.0,
+    // Bottom rack
+    -1.0, -0.7+0.65, 0.0,
+    -0.9, -0.6+0.65, 0.0,
+    -0.8, -0.7+0.65, 0.0,
+    -0.8, -0.7+0.65, 0.0,
+    -0.7, -0.6+0.65, 0.0,
+    -0.6, -0.7+0.65, 0.0,
+    -0.6, -0.7+0.65, 0.0,
+    -0.5, -0.6+0.65, 0.0,
+    -0.4, -0.7+0.65, 0.0,
+    -0.4, -0.7+0.65, 0.0,
+    -0.3, -0.6+0.65, 0.0,
+    -0.2, -0.7+0.65, 0.0,
+    -0.2, -0.7+0.65, 0.0,
+    -0.1, -0.6+0.65, 0.0,
+    0.0, -0.7+0.65, 0.0,
+    0.0, -0.7+0.65, 0.0,
+    0.1, -0.6+0.65, 0.0,
+    0.2, -0.7+0.65, 0.0,
+    0.2, -0.7+0.65, 0.0,
+    0.3, -0.6+0.65, 0.0,
+    0.4, -0.7+0.65, 0.0,
+    0.4, -0.7+0.65, 0.0,
+    0.5, -0.6+0.65, 0.0,
+    0.6, -0.7+0.65, 0.0,
+    0.6, -0.7+0.65, 0.0,
+    0.7, -0.6+0.65, 0.0,
+    0.8, -0.7+0.65, 0.0,
+    0.8, -0.7+0.65, 0.0,
+    0.9, -0.6+0.65, 0.0,
+    1.0, -0.7+0.65, 0.0,
+  ];
+
+  // Let time between 0 and 1,
+  // Linear interpolation between normal and long logo
+  var time = Math.abs(Math.sin(degToRad(theta*rate)));
+  for (var i = 0; i < triangleVertices.length; i++) {
+    triangleVertices[i] = lerp(triangleVerticesLong[i], triangleVertices[i], time);
+  }
+
+  vertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.DYNAMIC_DRAW);
+  vertexPositionBuffer.itemSize = 3;
+  vertexPositionBuffer.numberOfItems = triangleVertices.length / vertexPositionBuffer.itemSize;
+}
+
+/**
+ * Load colors to buffer
+ */
+function loadColorsMy() {
+  vertexColorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  var colors = [];
+
+  // First half of the buffer filled blue color,
+  // the other half filled orange color.
+  for (var i = 0; i < vertexPositionBuffer.numberOfItems; i++) {
+    colors.push(...[0.0, 0.0, 0.0, 1.0]);
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
+  vertexColorBuffer.itemSize = 4;
+  vertexColorBuffer.numItems = vertexPositionBuffer.numberOfItems;
+}
+
+/**
+ * Populate buffers with data
+ */
+function setupBuffersMy() {
+  //Generate the vertex positions    
+  loadVerticesMy();
+
+  //Generate the vertex colors
+  loadColorsMy();
 }
 
 /**
@@ -382,15 +611,41 @@ function degToRad(degrees) {
 }
 
 /**
+ * Draw logo animation
  * Draw call that applies matrix transformations to model and draws model in frame
  */
-function draw() {
+function drawLogo() {
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   mat4.identity(mvMatrix);
   mat4.scale(mvMatrix, mvMatrix, vec3.fromValues(Math.cos(degToRad(theta*rate)), Math.cos(degToRad(theta*rate)), 1));
   mat4.translate(mvMatrix, mvMatrix, vec3.fromValues(Math.sin(degToRad(theta*rate)), 0, 0));
+  mat4.identity(pMatrix);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+    vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
+    vertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+
+  setMatrixUniforms();
+
+  gl.drawArrays(gl.TRIANGLES, 0, vertexPositionBuffer.numberOfItems);
+}
+
+/**
+ * Draw my animation
+ */
+function drawMy() {
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  mat4.identity(mvMatrix);
   mat4.identity(pMatrix);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
@@ -418,17 +673,35 @@ function animate() {
     theta += 1.0;
   }
   lastTime = timeNow;
-  loadVertices();
+  if (logo) loadVerticesLogo();
+  else loadVerticesMy();
 }
 
 /**
  * Tick called for every animation frame.
  */
 function tick() {
-  type = document.getElementsByName("type");
+  var type = document.getElementsByName("type");
   requestAnimFrame(tick);
-  if (type[0].checked) draw();
-  else if (type[1].checked) ;
+  if (type[0].checked) {
+    if (logo != true) change = true;
+    logo = true;
+  } else if (type[1].checked) {
+    if (logo != false) change = true;
+    logo = false;
+  }
+
+  if (change) {
+    if (logo) {
+      setupBuffersLogo();
+    } else {
+      setupBuffersMy();
+    }
+  }
+
+  if (logo) drawLogo();
+  else drawMy();
+  
   animate();
 }
 
@@ -439,7 +712,7 @@ function startup() {
   canvas = document.getElementById("myGLCanvas");
   gl = createGLContext(canvas);
   setupShaders();
-  setupBuffers();
+  setupBuffersLogo();
   gl.clearColor(1.0, 1.0, 1.0, 1.0);
   // gl.enable(gl.DEPTH_TEST);
   tick();
